6dcc7ec05b5dd45e2b10256e3d2c06274ccea3a6
Felipe Kurjata <fkurjata@gmail.com>
2024-02-04 12:43:40 -0300

    melhorias de rota e required

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,20 +7,23 @@
         @csrf
         <div class="form-group">
             <label for="title">Título:</label>
-            <input type="text" name="title" id="title" class="form-control">
+            <input type="text" name="title" id="title" class="form-control" required>
         </div>
         
         <div class="form-group">
             <label for="description">Descrição:</label>
-            <input type="text" name="description" id="description" class="form-control">
+            <input type="text" name="description" id="description" class="form-control" required>
         </div>
         
         <div class="form-group">
             <label for="author">Autor:</label>
-            <input type="text" name="author" id="author" class="form-control">
+            <input type="text" name="author" id="author" class="form-control" required>
         </div>
         
-        <button type="submit" class="btn btn-primary">Adicionar</button>
+        <button type="submit" class="btn btn-success">Adicionar</button>
+        <br>
+        <br>
+        <a href="{{ url('/') }}" class="btn btn-primary">Voltar</a>
     </form>
 </div>
 @endsection
